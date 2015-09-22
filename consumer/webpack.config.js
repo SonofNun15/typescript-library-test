139fd5c27e9ec2a008ffb85c69c68874fcffefdf
Josh Graber <josh.graber.85@gmail.com>
2015-09-22 14:28:37 -0400

    Add a simple webpack configuration with ts-loader

---
+++ consumer/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+    entry: './source/app',
+    output: {
+		path: path.resolve('built'),
+		filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+				test: /\.ts$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
+        ],
+    },
+	resolve: {
+		extensions: ['.js', '.ts'],
+	},
+};
