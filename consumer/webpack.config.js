c8a868882f456a9fe87741a326627880c1fdc19e
Josh Graber <jgraber@renovo1.com>
2015-09-22 14:49:04 -0400

    Clean up whitespace

--- consumer/webpack.config.js
+++ consumer/webpack.config.js
@@ -1,20 +1,20 @@
 var path = require('path');
 
 module.exports = {
-    entry: './source/app',
-    output: {
+	entry: './source/app',
+	output: {
 		path: path.resolve('built'),
 		filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
+	},
+	module: {
+		loaders: [
+			{
 				test: /\.ts$/,
 				exclude: /node_modules/,
 				loader: 'ts-loader',
 			},
-        ],
-    },
+		],
+	},
 	resolve: {
 		extensions: ['.js', '.ts'],
 	},
